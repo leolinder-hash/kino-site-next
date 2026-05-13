760c7175ee5d9e958f20bed05af187255ba5f407
Hussam Hilow <developedbyhussam@gmail.com>
2026-05-13 12:59:19 +0200

    added hashing and comparison function for user password

--- models/User.js
+++ models/User.js
@@ -27,3 +27,12 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
 }
 );
+userSchema.pre("save", async function () {
+    if(this.isModified("password")) return;
+    this.password = await bcrypt.hash(this.password, 12);
+});
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+const User = mongoose.models.user || mongoose.model("user", userSchema);
+export default User;
