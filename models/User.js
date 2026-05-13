38b4cb81cdcd8ee72f72435b3ea0e3e5f8d8ca83
Hussam Hilow <developedbyhussam@gmail.com>
2026-05-13 12:56:26 +0200

    added data requirements for name, email and password

--- models/User.js
+++ models/User.js
@@ -8,4 +8,22 @@ const userSchema = new mongoose.Schema(
     required: [true, "Name is required"],
     trim: true,
     },
-    
+    email:{
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"],
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength:[8, "Password must be at least 8 characters"],
+        select: false,
+    },
+},
+{
+    timestamps: true,
+}
+);
