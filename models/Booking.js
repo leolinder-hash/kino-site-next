2e87bfa082127bd36620066d1c696b80d2be4988
Paul Ibrahim <paul.ibrahim@hotmail.se>
2026-05-13 13:30:40 +0200

    Created booking data model

---
+++ models/Booking.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    screeningId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Screening',
+        required: true
+    },
+    seats: {
+        type: [String],
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['confirmed', 'cancelled'],
+        default: 'confirmed'
+    },
+    bookingDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Booking= mongoose.models.Booking || mongoose.model('Booking', bookingSchema);
+export default Booking;
