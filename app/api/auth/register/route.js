dd66411337eefcf6fb0aa17cb925dbc47d1d780c
Hussam Hilow <developedbyhussam@gmail.com>
2026-05-13 14:19:17 +0200

    changed a naming error which resulted in status 500 error, from nextResponse to the correct NextResponse

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,4 +1,4 @@
-import {nextResponse} from 'next/server';
+import {NextResponse} from 'next/server';
 import {connectDB} from '@/lib/mongodb';
 import User from '@/models/User';
 
@@ -9,11 +9,11 @@ export async function POST(request) {
         await connectDB();
         const {name, email, password} = await request.json();
         if (!name || !email || !password) {
-            return nextResponse.json({success: false, message: "All fields are required"}, {status: 400});
+            return NextResponse.json({success: false, message: "All fields are required"}, {status: 400});
         }
         const user = await User.create({name, email, password});
 
-        return nextResponse.json({
+        return NextResponse.json({
             success: true,
             user:{
                 id: user._id,
@@ -23,8 +23,8 @@ export async function POST(request) {
          }, {status: 201});
     }catch (error) {
         if (error.code === 11000) {
-            return nextResponse.json({success: false, message: "Email already exists"}, {status: 400});
+            return NextResponse.json({success: false, message: "Email already exists"}, {status: 400});
         }
-        return nextResponse.json({success: false, message: error.message}, {status: 500});
+        return NextResponse.json({success: false, message: error.message}, {status: 500});
     }
 }
