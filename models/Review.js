57b7c526f3864edc5cbb66d0ad198b2797c5ded6
kriisa-studier <233270052+kriisa-studier@users.noreply.github.com>
2026-05-15 08:18:36 +0200

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -6,7 +6,7 @@ const reviewSchema = new Schema(
     {
         user: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'user',
+            ref: 'User',
             required: true,
         },
         movie: {
