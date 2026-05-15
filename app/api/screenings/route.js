543aa00d491af9809a2bc3d1d5a984a43479ca3b
ImanBatyah <iman.nagati@lerniastudent.se>
2026-05-15 17:46:27 +0200

    Create API routes for screenings

---
+++ app/api/screenings/route.js
@@ -0,0 +1,29 @@
+import { connectDB } from "../../../lib/mongodb.js";
+import Screening from "../../../models/screening.js";
+import Movie from "../../../models/Movie.js";
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const screenings = await Screening.find().populate("movie");
+
+    return Response.json(
+      {
+        success: true,
+        count: screenings.length,
+        screenings,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return Response.json(
+      {
+        success: false,
+        message: "Could not fetch screenings",
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ models/screening.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+// create embedded array as sub schema for seats
+// have to be generated when creating a screening
+// IMPORTANT: booking.js stores seat as string not as object,
+// so API would have to enable conversion between both formats.
+
+const SeatSchema = new mongoose.Schema({
+  row: { type: String, required: true },
+  number: { type: Number, required: true },
+  isBooked: { type: Boolean, default: false },
+});
+
+const ScreeningSchema = new mongoose.Schema(
+  {
+    movie: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Movie', // links to Movie id from movie.js
+      required: true,
+    },
+    startTime: {
+      type: Date,
+      required: true,
+    },
+    room: {
+      type: String,
+      required: true,
+    },
+    seats: [SeatSchema], // embedded array of all seats in the room. BOOKING SAVES THIS AS STRING, API CONVERTS TO OBJECT
+    totalSeats: {
+      type: Number,
+      required: true,
+    },
+    availableSeats: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Screening ||
+  mongoose.model('Screening', ScreeningSchema);
