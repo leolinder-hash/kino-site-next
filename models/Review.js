27919b59dbd2ea5360435d3ce7e10132f28a496c
kriisa-studier <233270052+kriisa-studier@users.noreply.github.com>
2026-05-15 08:17:40 +0200

    Created review data model

---
+++ models/Review.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const { Schema, model, models } = mongoose;
+
+const reviewSchema = new Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user',
+            required: true,
+        },
+        movie: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Movie',
+            required: true,
+        },
+        rating: {
+            type: Number,
+            required: true,
+            min: [1, 'The rating cannot be lower than 1'],
+            max: [5, 'The rating cannot be higher than 5'],
+            validate: {
+                validator: Number.isInteger,
+                message: 'The rating must be a whole number.'
+            }
+        },
+        reviewText: {
+            type: String,
+            required: true,
+            trim: true,
+            maxLength: [1000, 'The review cannot exceed 1000 characters.']
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+reviewSchema.index({ user: 1, movie: 1 }, { unique: true });
+
+const Review = models.Review || model('Review', reviewSchema);
+
+export default Review;
