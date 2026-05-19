5784436a902251fbde644d4ad81fa46cfa9c73d7
Paul Ibrahim <paul.ibrahim@hotmail.se>
2026-05-19 19:41:52 +0200

    Add create booking API

--- models/Booking.js
+++ models/Booking.js
@@ -1,16 +1,43 @@
 import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema({
+
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true
+        required: false
     },
+
+    guestName: {
+        type: String,
+        required: function () {
+            return !this.userId;
+        },
+        trim: true
+    },
+
+    guestEmail: {
+        type: String,
+        required: function () {
+            return !this.userId;
+        },
+        trim: true
+    },
+
+    guestPhoneNumber: {
+        type: String,
+        required: function () {
+            return !this.userId;
+        },
+        trim: true
+    },
+
     screeningId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Screening',
         required: true
     },
+
     seats: {
         type: [String],
         required: true,
@@ -18,14 +45,28 @@ const bookingSchema = new mongoose.Schema({
             validator: function (value) {
                 return value.length > 0;
             },
-            message: "At least one seat must be selected."
+            message: "At least one seat must be selected"
         }
     },
+
+    deliveryMethod: {
+        type: String,
+        enum: ["email", "sms", "pickup"],
+        default: "email"
+    },
+
+    paymentMethod: {
+        type: String,
+        enum: ["card", "swish", "giftcard", "counter"],
+        default: "card"
+    },
+
     status: {
         type: String,
         enum: ['confirmed', 'cancelled'],
         default: 'confirmed'
     },
+
     bookingDate: {
         type: Date,
         default: Date.now
