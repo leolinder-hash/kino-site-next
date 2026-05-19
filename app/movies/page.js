8d239351036eaab03881958979bf466d295689b3
leolinder-hash <leolinder074@gmail.com>
2026-05-19 14:44:21 +0200

    Render movies using movies API route

--- app/movies/page.js
+++ app/movies/page.js
@@ -1,11 +1,20 @@
 import MovieCard from "@/components/MovieCard"
 
-export default function MoviePage() {
+export default async function MoviePage() {
+  const url = "http://localhost:3000/api/movies";
+
+  const response = await fetch(url);
+  const { movies } = await response.json();
+
+
   return (
     <div>
       <h1>Alla filmer</h1>
-      <MovieCard />
+      <div>{movies.map((movie => (
+        <MovieCard key={movie._id} movie={movie} />
+      )))
+      }
+      </div>
     </div>
-
   )
 }
