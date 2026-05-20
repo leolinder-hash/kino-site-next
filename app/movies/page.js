ff060c9823b03a8f5fb1bfd267bd01fad4ddf932
leolinder-hash <leolinder074@gmail.com>
2026-05-20 17:45:42 +0200

    Add fallback message if movies cannot be fetched

--- app/movies/page.js
+++ app/movies/page.js
@@ -2,21 +2,30 @@ import MovieCard from "@/components/MovieCard";
 import styles from "./page.module.scss";
 
 export default async function MoviePage() {
-  const url = "http://localhost:3000/api/movies";
+  try {
+    const url = "http://localhost:3000/api/movies";
 
-  const response = await fetch(url);
-  const { movies } = await response.json();
+    const response = await fetch(url);
 
+    if (!response.ok) {
+      throw new Error("Failed to fetch movies");
+    }
 
-  return (
-    <div className={styles.movies__page}>
-      <h1>Våra filmer</h1>
-      <section className={styles.movies__grid}>
-        {movies.map((movie => (
-          <MovieCard key={movie._id} movie={movie} />
-        )))
-        }
-      </section>
-    </div>
-  )
+    const { movies } = await response.json();
+
+    return (
+      <div className={styles.movies__page}>
+        <h1>Våra filmer</h1>
+        <section className={styles.movies__grid}>
+          {movies.map((movie => (
+            <MovieCard key={movie._id} movie={movie} />
+          )))
+          }
+        </section>
+      </div>
+    )
+  } catch (error){
+    console.log(error);
+    return <p>Movies could not load</p>
+  }
 }
