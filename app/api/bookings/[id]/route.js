9a2b4a1303fbac08e855bd0710becb709f450b58
Paul Ibrahim <paul.ibrahim@hotmail.se>
2026-05-23 21:39:46 +0200

    Merge main into delete booking branch

--- app/api/bookings/[id]/route.js
+++ app/api/bookings/[id]/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
 import Booking from "@/models/Booking";
+import Screening from "@/models/Screening";
 
 export async function DELETE(req, { params }) {
     try {
@@ -19,9 +20,25 @@ export async function DELETE(req, { params }) {
             }, { status: 404 });
         }
 
+        const screening= await Screening.findById(booking.screeningId);
+       
+        booking.seats.forEach(bookedSeat => {
+            const seat = screening.seats.find((seat) => {
+                const seatCode = `${seat.row}${seat.number}`;
+                return seatCode === bookedSeat;
+            })
+
+            if (seat) {
+                seat.isBooked = false;
+            }
+        });
+
+        screening.availableSeats += booking.seats.length;
+        await screening.save();
+
         return NextResponse.json({
             success: true,
-            message: "Booking cancelled", booking
+            message: "Booking cancelled and seats released", booking
         });
 
     } catch (error) {
