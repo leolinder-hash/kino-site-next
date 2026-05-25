682dfd74e74a9bb59f77d8d2a4ca4b8d263e7aec
Paul Ibrahim <paul.ibrahim@hotmail.se>
2026-05-25 15:50:23 +0200

    Mark screening seats as booked when creating booking

--- app/api/bookings/route.js
+++ app/api/bookings/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
 import Booking from "@/models/Booking";
+import Screening from "@/models/Screening";
 
 export async function POST(req) {
     try {
@@ -28,6 +29,22 @@ export async function POST(req) {
                 error: "One or more selected seats are already booked for this screening."
             }, { status: 409 });
         }
+
+        const screening = await Screening.findById(screeningId);
+
+        seats.forEach((selectedSeat) => {
+            const seat = screening.seats.find((seat) => {
+                const seatCode = `${seat.row}${seat.number}`;
+                return seatCode === selectedSeat;
+            });
+
+            if (seat) {
+                seat.isBooked = true;
+            }
+        })
+
+        screening.availableSeats -= seats.length;
+        await screening.save();
 
         const booking = await Booking.create({
             userId,
