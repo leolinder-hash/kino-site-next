2198912e271bd247d858a8bb9e413e09721c7d7e
leolinder-hash <leolinder074@gmail.com>
2026-05-26 13:18:17 +0200

    Update error handling for movies

--- app/movies/[id]/page.js
+++ app/movies/[id]/page.js
@@ -1,24 +1,19 @@
+import { notFound } from "next/navigation";
 import MovieDetail from "@/components/MovieDetail";
 
 export default async function MovieDetailPage({ params }) {
-  try {
-    const { id } = await params;
-    const url = `http://localhost:3000/api/movies/${id}`;
+  const { id } = await params;
+  const url = `http://localhost:3000/api/movies/${id}`;
 
-    const response = await fetch(url);
+  const response = await fetch(url);
 
-    if (!response.ok) {
-      throw new Error("Failed to fetch movie");
-    }
-
-    const { movie } = await response.json();
-
-    return (
-      <MovieDetail movie={movie} />
-    )
-
-  } catch (error) {
-    console.log(error);
-    return <p>Movie could not be found</p>
+  if (!response.ok) {
+    notFound();
   }
+
+  const { movie } = await response.json();
+
+  return (
+    <MovieDetail movie={movie} />
+  )
 }
--- app/movies/page.js
+++ app/movies/page.js
@@ -2,13 +2,12 @@ import MovieList from "@/components/MovieList";
 import styles from "./page.module.scss";
 
 export default async function MoviePage() {
-  try {
     const url = "http://localhost:3000/api/movies";
 
     const response = await fetch(url);
 
     if (!response.ok) {
-      throw new Error("Failed to fetch movies");
+      return <p>Movies could not load</p>
     }
 
     const { movies } = await response.json();
@@ -21,8 +20,4 @@ export default async function MoviePage() {
         <MovieList movies={movies} />
       </>
     )
-  } catch (error) {
-    console.log(error);
-    return <p>Movies could not load</p>
-  }
 }
